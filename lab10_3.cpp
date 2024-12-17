e42a974512835a2a392ec63e452ec27d9ba2cd72
DUNRUZEED <159751313+FIRSTDRZ@users.noreply.github.com>
2024-12-17 16:24:10 +0700

    lad10_3

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include<fstream>
+#include <cmath>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    ofstream dest ;
+    ifstream source;
+    source.open("score.txt");
+    int count = 0;
+    float sum = 0;
+
+    float standard  = 0;
+    string text;
+    
+    while(getline(source,text)){
+        count += 1;
+        sum += atof(text.c_str());
+        standard += pow(stof(text),2);
+    }
+    cout << "Number of data = " << count << "\n";
+    cout << setprecision(3);
+    double mean = sum/count;
+    double all = standard/count;
+    cout << "Mean = " << mean << "\n";
+    cout << "Standard deviation = " << pow(all - (pow(mean,2)),0.5) ;
+}
